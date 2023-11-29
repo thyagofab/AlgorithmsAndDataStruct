5e867d3dd3e08d0857b9739038b44870cc6be296
Thyago Fabricio <thyago.fabricio2015@hotmail.com>
2023-11-29 19:10:57 -0300

    Update list1.c
    ajeitando alguns erros

--- list1.c
+++ list1.c
@@ -1,26 +1,36 @@
 #include <stdio.h>
 
-int numb(int vet[5]){
-    for (int i = 0; i < 5; i++){
+int numb(int n, int vet[n])
+{
+
+    for (int i = 0; i < n; i++)
+    {
         vet[i] = i;
     }
 
-    return vet[4];
+    return vet[n - 1];
 }
-void my_fuct(int vet[5]){
 
-    for (int i = 4; i >= 0; i--){
-        printf("%d", vet[i]);
+void my_fuct(int n, int vet[])
+{
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        printf("%d ", vet[i]);
     }
 
     printf("\n");
-
 }
 
-int main(){
+int main()
+{
 
-    int principal_vet[5];
-    numb(principal_vet);
-    my_fuct(principal_vet);
+    int n;
+    printf("DIGITE UM VALOR DE QUANTIDADE N DO TAMANHO QUE VOCÊ DESEJA: ");
+    scanf("%d", &n);
+
+    int principal_vet[n];
+
+    numb(n, principal_vet);
+    my_fuct(n, principal_vet);
 }
-
