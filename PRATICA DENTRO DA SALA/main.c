5457a1548be7462a28432a899d6ae0f1a8cb5f8d
Thyago Fabricio <thyago.fabricio2015@hotmail.com>
2023-12-04 18:00:11 -0300

    ajeitando atv

--- PRATICA DENTRO DA SALA/main.c
+++ PRATICA DENTRO DA SALA/main.c
@@ -1,36 +1,67 @@
 #include <stdio.h>
 
-int soma(int a, int b){
+int soma(int a, int b)
+{
     return a + b;
 }
 
-int sub(int a, int b){
+int subtracao(int a, int b)
+{
     return a - b;
 }
-int mut(int a, int b){
+int mutiplicao(int a, int b)
+{
     return a * b;
 }
 
-int div(int a, int b){
-    return a/b;
+int divisao(int a, int b)
+{
+    return a / b;
 }
 
-int calcula(int x, int y, int(*operacao)(int, int)){
-    return(*operacao)(x,y);
+int calcula(int x, int y, int (*operacao)(int, int))
+{
+    return (*operacao)(x, y);
 }
 
-int main(void){
-    int resultado = calcula(5,3,sub);
-    printf("Resultado subtração: %d\n", resultado);
 
-    int resultado1 = calcula(5,3,soma);
-    printf("Resultado da soma: %d\n", resultado1);
+int main(void)
+{
+    int escolha, resultado, numero1, numero2;
+    printf("1-soma\n"
+           "2-subtração\n"
+           "3-multiplicação\n"
+           "4-divisão\n");
+    printf("Escolha uma operação:");
+    scanf("%d", &escolha);
 
-    int resultado2 = calcula(5,3,mut);
-    printf("Resultado da multiplicação: %d\n", resultado2);
+    printf("DIGITE O PRIMEIRO NUMERO: ");
+    scanf("%d", &numero1);
 
-    int resultado3 = calcula(9,3,div);
-    printf("Resultado da divisão: %d\n", resultado3);
+    printf("DIGITE O SEGUNDO NUMERO: ");
+    scanf("%d", &numero2);
+
+    switch (escolha)
+    {
+    case 1:
+        resultado = calcula(numero1, numero2, soma);
+        break;
+    
+    case 2:
+        resultado = calcula(numero1,numero2,subtracao);
+        break;
+    
+    case 3:
+        resultado = calcula(numero1,numero2,mutiplicao);
+        break;
+    
+    case 4:
+        resultado = calcula(numero1,numero2,divisao);
+        break;
+    default:
+        printf("OPERAÇÃO INVALIDA");
+        break;
+    }
+    printf("Resultado: %d", resultado);
     return 0;
-
 }
