e40ead985a7f62ec27a3113b8ebccf2d1618c827
Thyago Fabricio <thyago.fabricio2015@hotmail.com>
2023-12-09 15:12:37 -0300

    andamento das questoes

--- ATIVIDADE_DE_ALOCACAO_DE_MEMORIA/quest2.c
+++ ATIVIDADE_DE_ALOCACAO_DE_MEMORIA/quest2.c
@@ -2,7 +2,57 @@
 #include <stdlib.h>
 
 
+int resultado(char *gabarito, char *resposta_do_aluno,int total_de_questoes){
+    int total_de_acertos = 0;
 
-int main(void){
+    for (int i = 0; i < total_de_questoes; i++)
+    {
+        if (gabarito[i] == resposta_do_aluno[i])
+        {
+            total_de_acertos++;
+        }
+    }
+    return total_de_acertos;
+}
 
+
+
+int main(void)
+{
+    int total_de_questoes;
+    int total_de_acertos = 0;
+
+    
+    printf("DIGITE O TOTAL DE QUESTÕES: ");
+    scanf("%d", &total_de_questoes);
+
+
+    
+    char *gabarito = (char *)malloc(total_de_questoes * sizeof(char));
+    char **resposta_do_aluno = (char *)malloc(total_de_questoes * sizeof(char));
+
+    if (gabarito == NULL && resposta_do_aluno == NULL)
+    {
+        printf("ERRO DE ALOCAÇÃO DE MEMORIA\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < total_de_questoes; i++) {
+
+        printf("DIGITE A RESPOSTA DA QUESTÃO %d: ", i + 1);
+        scanf(" %c", &gabarito[i]);
+    }
+
+    for (int i = 0; i < total_de_questoes; i++) {
+        printf("DIGITE A RESPOSTA DO ALUNO PARA A QUESTÃO %d: ", i + 1);
+        scanf(" %c", &resposta_do_aluno[i]);
+    }
+
+    total_de_acertos = resultado(gabarito, resposta_do_aluno, total_de_questoes);
+
+    printf("O TOTAL DE ACERTOS FOI: %d\n", total_de_acertos);
+
+    free(gabarito);
+    free(resposta_do_aluno);
+    return 0;
 }
--- PRATICA DENTRO DA SALA/alocacao_da_matriz.c
+++ PRATICA DENTRO DA SALA/alocacao_da_matriz.c
@@ -2,13 +2,31 @@
 #include <stdlib.h>
 
 int main(){
-    int linhas = 3, colunas = 3, i, j;
+    int linha = 5 , coluna = 5;
+    
+    int **matriz = (int **) malloc(linha * sizeof(int *));
 
-    int **matriz = (int **) malloc(linhas*sizeof(int*));
-    for (i = 0; i < linhas; i++){
-        matriz[i] = (int *) malloc(colunas *sizeof(int));
+    for (int i = 0; i < linha; i++){
+        matriz[i] = (int *) malloc(coluna * sizeof(int *));
+    }
+
+    for (int i = 0; i < linha; i++){
+        for (int j = 0; j < coluna; j++){
+            matriz[i][j] = (i*5) +j;
+        }
     }
 
 
-    
+    for(int i = 0; i < linha; i++){
+        for (int j = 0; j < coluna; j++){
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < linha; i++){
+        free(matriz[i]);
+    }
+
+    free(matriz);
 }
