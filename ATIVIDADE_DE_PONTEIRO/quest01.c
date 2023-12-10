5ccb225879eca580cd3a8b1cc6378365e3359a5f
Thyago Fabricio <143232809+thyagofab@users.noreply.github.com>
2023-12-09 22:14:10 -0300

    Update quest01.c

--- ATIVIDADE_DE_PONTEIRO/quest01.c
+++ ATIVIDADE_DE_PONTEIRO/quest01.c
@@ -10,6 +10,5 @@ int main(){
     --x;
     (*p)+=x;
 }
-
-/*O valores que vão retornar em x,y e p são respectivamente y = 4 e x = 3 p = 4
-*/
+ 
+/*O valores que vão retornar em x,y e p são respectivamente y = 4 e x = 3 p = 0x7fffca03739c ele volta o endereço de y*/
