f586db2a3e358b1842ab9d5a89e700b7d2994ed5
Thyago Fabricio <thyago.fabricio2015@hotmail.com>
2024-02-03 15:48:51 -0300

    atv

--- 2 - Unidade/ATIVIDADE MANIPULACAO DE ARQUIVO/quest1.c
+++ 2 - Unidade/ATIVIDADE MANIPULACAO DE ARQUIVO/quest1.c
@@ -1,24 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int main(void) {
+    FILE *saida_q1, *entrada_q1;
+    float nota1, nota2, nota3, media;
+    char nome[20];
+    
+    entrada_q1 = fopen("entrada_q1.txt", "r");
+    saida_q1 = fopen("saida_q1.txt", "w");
 
-int main(void){
-    FILE *arquivo_de_entrada;
-    FILE *arquivo_de_saida;
-
-    arquivo_de_saida = fopen("saida_q3.txt","rt");
-    arquivo = fopen("Entrada_q3.txt", "W") ;
-
-        if(arquivo == NULL && arquivo_de_saida == NULL){
-        printf("Erro de abertura de arquivo! \n");
+    if (entrada_q1 == NULL || saida_q1 == NULL) {
+        printf("Erro ao abrir o arquivo");
         exit(1);
+    } else {
+        printf("Arquivos abertos com sucesso\n");
     }
 
-    arquivo_de_entrada =  
-    fgets(c, 20, arquivo); // ler um linha do arquivo
-    printf("%s\n", c);
+// o while funciona da seguinte forma: vai ler o arquivo até o final do arquivo com != EOF, assim lendo cada dado do arquivo e atribuindo para cada variavel.
+// o formato de entrada significa da seguinte forma: %19[^\t]%*[\t] ele vai as 19 caractere desctando \o até encontra \t.
+    while (fscanf(entrada_q1, "%19[^\t]%*[\t]%f%f%f", nome, &nota1, &nota2, &nota3) != EOF) {
+        media = (nota1 + nota2 + nota3) / 3;
 
+        printf("Nome: %s, Nota 1: %.1f, Nota 2: %.1f, Nota 3: %.1f, Média: %.1f\n", nome, nota1, nota2, nota3, media); // esse printf foi feito para verificar se foi feito a leitura dos dados!
+        fprintf(saida_q1, "%s\t%.1f\t%s\n", nome, media, (media >= 7.0) ? "aprovado" : "reprovado"); // aqui está escrevendo no arquivo de saida cada resultado.
 
+    }
+
+    fclose(entrada_q1);
+    fclose(saida_q1);
 
     return 0;
 }
---
+++ 2 - Unidade/ATIVIDADE MANIPULACAO DE ARQUIVO/quest2.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    int vetor[10];
+    int i = 0;
+    int maior, menor;
+    float media;
+    FILE *entrada_q2, *saida_q2;
+
+    entrada_q2 = fopen("Entrada_q2.txt","r");
+    saida_q2 = fopen("Saida_q2.txt","w");
+
+    if (entrada_q2 == NULL || saida_q2 == NULL) {
+        printf("Erro ao abrir o arquivo");
+        exit(1);
+    } else {
+        printf("Arquivos abertos com sucesso\n");
+    }
+
+    while(fscanf(entrada_q2,"%d\n",&vetor[i]) != EOF){
+        media += vetor[i];
+        i++;
+    }
+
+
+    maior = vetor[0];
+    menor = vetor[0];
+
+    for (int i = 0; i < 9; i++){
+        if(vetor[i] > maior){
+            maior = vetor[i];
+        }
+        if(vetor[i] < menor){
+            menor = vetor[i];
+        }
+    }
+
+
+    fprintf(saida_q2, "Maior: %d\nMenor: %d\nMedia: %.2f",maior, menor, media / 10.0);
+
+
+    fclose(entrada_q2);
+    fclose(saida_q2);
+
+    return 0;
+}
