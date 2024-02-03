fcfb80c596e4fdb777b1372b857d654820c92fe9
Thyago Fabricio <thyago.fabricio2015@hotmail.com>
2024-02-03 12:28:11 -0300

    atv

--- 2 - Unidade/ATIVIDADE MANIPULACAO DE ARQUIVO/quest1.c
+++ 2 - Unidade/ATIVIDADE MANIPULACAO DE ARQUIVO/quest1.c
@@ -3,11 +3,10 @@
 
 
 int main(void){
-    FILE *arquivo;
+    FILE *arquivo_de_entrada;
     FILE *arquivo_de_saida;
 
     arquivo_de_saida = fopen("saida_q3.txt","rt");
-
     arquivo = fopen("Entrada_q3.txt", "W") ;
 
         if(arquivo == NULL && arquivo_de_saida == NULL){
@@ -15,9 +14,9 @@ int main(void){
         exit(1);
     }
 
+    arquivo_de_entrada =  
     fgets(c, 20, arquivo); // ler um linha do arquivo
     printf("%s\n", c);
-
 
 
 
